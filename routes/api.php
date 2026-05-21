e38ac0035c1b8376bd0c79ad984f9379ccdd2672
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-05-21 16:59:41 +0200

    feat: create the nfc Card controller that contain's methods: index to show all nfcCards, store to the create an nfcCard, assign to affect a nfc card to a specific server, toggle to activate and desactivate the nfccard, and finally the destroy the nfc card

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServerController;
+use App\Http\Controllers\Api\NfcCardController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -21,5 +22,14 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/servers/{id}', [ServerController::class, 'update']);
         Route::delete('/servers/{id}', [ServerController::class, 'destroy']);
         });
+        // nfc card management
+         Route::get('/nfc-cards', [NfcCardController::class, 'index']);
+        Route::post('/nfc-cards', [NfcCardController::class, 'store']);
+
+        Route::post('/nfc-cards/{id}/assign', [NfcCardController::class, 'assign']);
+
+        Route::patch('/nfc-cards/{id}/toggle', [NfcCardController::class, 'toggle']);
+
+        Route::delete('/nfc-cards/{id}', [NfcCardController::class, 'destroy']);
 });
 
