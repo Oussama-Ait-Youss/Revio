e8e0fb2de674ca055d4a48708b0f997181785dbd
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-05-21 16:50:17 +0200

    feat: create isadmin middelware to give the admin the posibility to see update create delete servers

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,19 @@ use App\Http\Controllers\Api\ServerController;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    // auth routes
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+
+    
+    // server management
+    Route::middleware('is_admin')->group(function (){
+        Route::get('/servers', [ServerController::class, 'index']);
+        Route::get('/servers/{id}', [ServerController::class, 'show']);
+        Route::post('/servers', [ServerController::class, 'store']);
+        Route::put('/servers/{id}', [ServerController::class, 'update']);
+        Route::delete('/servers/{id}', [ServerController::class, 'destroy']);
+        });
 });
 
-// server routes
-
-Route::get('/servers', [ServerController::class, 'index']);
-Route::get('/servers/{id}', [ServerController::class, 'show']);
-Route::post('/servers', [ServerController::class, 'store']);
-Route::put('/servers/{id}', [ServerController::class, 'update']);
-Route::delete('/servers/{id}', [ServerController::class, 'destroy']);
