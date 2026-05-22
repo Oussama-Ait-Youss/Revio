695d72556c1283f8fb6c9bdaaa2fc1d98fe7bd07
MoAmiine <medamine.teljaoui@gmail.com>
2026-05-22 17:30:21 +0100

    added server Model with relations and fillable

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Server extends Model
 {
-    //
+    protected $fillable = ['user_id', 'phone', 'total_reviews'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function nfcCards()
+    {
+        return $this->hasMany(NfcCard::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
